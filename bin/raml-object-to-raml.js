8b43af2eb1f10de09bb21b1b10f00069eb494cd1
Blake Embrey <hello@blakeembrey.com>
2014-09-16 16:21:21 -0600

    Fix require path for script

--- bin/raml-object-to-raml.js
+++ bin/raml-object-to-raml.js
@@ -1,5 +1,5 @@
 var concat = require('concat-stream');
-var toRAML = require('./');
+var toRAML = require('../');
 
 process.stdin.pipe(concat(function (data) {
   console.log(toRAML(JSON.parse(data.toString())));
